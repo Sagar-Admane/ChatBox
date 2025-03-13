da69e634b60548be85f0b867d9d6b6d1a7d0bd0e
Sagar Admane <118822902+Sagar-Admane@users.noreply.github.com>
2025-03-13 17:30:29 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,8 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/",
   plugins: [react()],
-  server: {
-    allowedHosts: ['11b9-112-196-62-6.ngrok-free.app']
-  }
-})
+});
